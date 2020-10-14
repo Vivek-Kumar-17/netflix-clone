84b9b07a10fa45121ffc47120996edbf7d293a13
Vivek Kumar <vivekkumar171000@gmail.com>
2020-10-14 10:41:55 +0530

    Updates

---
+++ precache-manifest.145c2bf262e9008f80ce0419228faf09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa34c9230ca51a48e63330dccb137fec",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "7094f9e52715b2e001cf",
+    "url": "/netflix-clone/static/css/main.001904da.chunk.css"
+  },
+  {
+    "revision": "49daf5637eab624484c1",
+    "url": "/netflix-clone/static/js/2.1357f758.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.1357f758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7094f9e52715b2e001cf",
+    "url": "/netflix-clone/static/js/main.f0023a6f.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
